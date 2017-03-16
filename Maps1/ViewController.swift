45eb4275a5c80483e0bdd9771cd5e8157a508f40
nvithala <vithala.niharika@gmail.com>
2017-03-16 15:38:56 -0400

    first commit

---
+++ Maps1/ViewController.swift
@@ -0,0 +1,153 @@
+//
+//  ViewController.swift
+//  Maps1
+//
+//  Created by Vithala,Niharika on 3/13/17.
+//  Copyright © 2017 Vithala,Niharika. All rights reserved.
+//
+
+import UIKit
+import GoogleMaps
+
+class ViewController: UIViewController{
+    
+    let baseURLDirections = "https://maps.googleapis.com/maps/api/directions/json?"
+    @IBOutlet weak var origin: UITextField!
+    @IBOutlet weak var destination: UITextField!
+    @IBOutlet weak var dummy: UIButton!
+    @IBOutlet weak var mapView1:GMSMapView!
+    @IBOutlet weak var distance: UITextField!
+    @IBOutlet weak var duration: UITextField!
+    
+    var selectedRoute: Dictionary<NSObject,AnyObject>!
+    var notSelectedRoute: Array<Dictionary<NSObject, AnyObject>> = []
+    var overviewPolyline: Dictionary<NSObject,AnyObject>!
+    var originCoordinate: CLLocationCoordinate2D!
+    var destinationCoordinate: CLLocationCoordinate2D!
+    var originAddress: String!
+    var destinationAddress: String!
+    var totalDistanceInMeters: UInt = 0
+    var totalDistance: String!
+    var totalDurationInSeconds: UInt = 0
+    var totalDuration: String!
+    
+    var originMarker: GMSMarker!
+    var destinationMarker: GMSMarker!
+    var routePolyline: GMSPolyline!
+    
+    override func viewDidLoad() {
+        let camera: GMSCameraPosition = GMSCameraPosition.cameraWithLatitude(48.857165, longitude: 2.354613, zoom: 8.0)
+        mapView1.camera = camera
+        print("loadeed camera")
+    }
+    
+    @IBAction func handleOrigin(sender: AnyObject) {
+        var sourceStr: String = origin.text!
+        var destinationStr: String = destination.text!
+        var error: NSError?
+        
+        var directionsURl = baseURLDirections+"origin="+sourceStr+"&destination="+destinationStr+"&alternatives=true"
+        directionsURl = directionsURl.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())!
+        
+        let finalURL = NSURL(string: directionsURl)
+        print("final url")
+        let directionsData = NSData(contentsOfURL: finalURL!)
+        print("directions data")
+        dispatch_async(dispatch_get_main_queue(),{ () -> Void in
+            //let directionsData = NSData(contentsOfURL: finalURL!)
+            print(" in here")
+            // to deserialize the data.
+            do {
+                if let dictionary = try NSJSONSerialization.JSONObjectWithData(directionsData!, options: .MutableContainers) as? Dictionary<NSObject,AnyObject> {
+                    print("check do")
+                    if(error != nil){
+                        print(error)
+                    } else {
+                        let status = dictionary["status"] as! String
+                        if status == "OK" {
+                            self.notSelectedRoute = (dictionary["routes"] as! Array<Dictionary<NSObject, AnyObject>>)
+                            for item in self.notSelectedRoute {
+                                self.overviewPolyline = item["overview_polyline"] as! Dictionary<NSObject, AnyObject>
+                                let legs = item["legs"] as! Array<Dictionary<NSObject, AnyObject>>
+                                
+                                let startLoc = legs[0]["start_location"] as! Dictionary<NSObject, AnyObject>
+                                self.originCoordinate =  CLLocationCoordinate2DMake(startLoc["lat"] as! Double, startLoc["lng"] as! Double)
+                                
+                                let endLoc = legs[legs.count - 1]["end_location"] as! Dictionary<NSObject, AnyObject>
+                                self.destinationCoordinate = CLLocationCoordinate2DMake(endLoc["lat"] as! Double, endLoc["lng"] as! Double)
+                                
+                                self.originAddress = legs[0]["start_address"] as! String
+                                self.destinationAddress = legs[legs.count - 1]["end_address"] as! String
+                                
+                                print("origin address\(self.originAddress) destination \(self.destinationAddress)")
+                                
+                                self.calculateTotalDistanceAndDuration(item)
+                                self.configureMapAndMarkersForRoute()
+                                self.drawRoute()
+                            }
+                            
+                        }
+    
+                    }
+                }
+            } catch {
+                print(error)
+            }
+        })
+    }
+    
+    func calculateTotalDistanceAndDuration(dum:Dictionary<NSObject,AnyObject>) {
+        
+        let legs = dum["legs"]as! NSArray
+        
+        totalDistanceInMeters = 0
+        totalDurationInSeconds = 0
+        
+        for step in legs {
+            totalDistanceInMeters += (step["distance"] as! Dictionary<NSObject, AnyObject>)["value"] as! UInt
+            totalDurationInSeconds += (step["duration"] as! Dictionary<NSObject, AnyObject>)["value"] as! UInt
+        }
+        
+        
+        let distanceInKilometers: Double = Double(totalDistanceInMeters / 1000)
+        totalDistance = "Total Distance: \(distanceInKilometers) Km"
+        
+        
+        let mins = totalDurationInSeconds / 60
+        let hours = mins / 60
+        let days = hours / 24
+        let remainingHours = hours % 24
+        let remainingMins = mins % 60
+        let remainingSecs = totalDurationInSeconds % 60
+        
+        totalDuration = "Duration: \(days) d, \(remainingHours) h, \(remainingMins) mins, \(remainingSecs) secs"
+        distance.text = totalDistance
+        duration.text = totalDuration
+        print(totalDuration)
+        print(totalDistance)
+    }
+    
+    func configureMapAndMarkersForRoute() {
+        mapView1.camera = GMSCameraPosition.cameraWithTarget(self.originCoordinate, zoom: 9.0)
+        originMarker = GMSMarker(position: self.originCoordinate)
+        originMarker.map = self.mapView1
+        originMarker.icon = GMSMarker.markerImageWithColor(UIColor.greenColor())
+        originMarker.title = self.originAddress
+        
+        destinationMarker = GMSMarker(position: self.destinationCoordinate)
+        destinationMarker.map = self.mapView1
+        destinationMarker.icon = GMSMarker.markerImageWithColor(UIColor.redColor())
+        destinationMarker.title = self.destinationAddress
+    }
+    
+    func drawRoute() {
+        let route = self.overviewPolyline["points"] as! String
+        
+        let path: GMSPath = GMSPath(fromEncodedPath: route)!
+        routePolyline = GMSPolyline(path: path)
+        routePolyline.map = mapView1
+    }
+
+}
+
+
